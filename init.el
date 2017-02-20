fd9c75e26c68217d1c28d2803aec5f9a44e09ac3
Debajit Adhikary <debajit@debajit.com>
2017-02-19 20:47:01 -0800

    Add spellcheck support

--- init.el
+++ init.el
@@ -321,6 +321,16 @@
   :config
   (add-hook 'after-change-major-mode-hook 'fci-mode))
 
+;; Spellcheck with flyspell
+(use-package flyspell
+  :config
+  (when (eq system-type 'windows-nt)
+    (add-to-list 'exec-path "C:/Program Files (x86)/Aspell/bin/"))
+  (setq ispell-program-name "aspell" ; use aspell instead of ispell
+        ispell-extra-args '("--sug-mode=ultra"))
+  (add-hook 'text-mode-hook #'flyspell-mode)
+  (add-hook 'prog-mode-hook #'flyspell-prog-mode))
+
 ;; HAML mode
 (use-package haml-mode
   :ensure t)

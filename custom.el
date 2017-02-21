274ac0cd6c4dba6f1e538b7adf04e6c733af54cf
Debajit Adhikary <debajit@debajit.com>
2017-02-20 20:05:17 -0800

    Add custom.el config for org mode typography

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
  '(ansi-color-names-vector
    ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
  '(ansi-term-color-vector
-   [unspecified "#1F1611" "#660000" "#144212" "#EFC232" "#5798AE" "#BE73FD" "#93C1BC" "#E6E1DC"])
+   [unspecified "#1F1611" "#660000" "#144212" "#EFC232" "#5798AE" "#BE73FD" "#93C1BC" "#E6E1DC"] t)
  '(custom-safe-themes
    (quote
     ("cbd8e65d2452dfaed789f79c92d230aa8bdf413601b261dbb1291fb88605110c" "6145e62774a589c074a31a05dfa5efdf8789cf869104e905956f0cbd7eda9d0e" "5a7830712d709a4fc128a7998b7fa963f37e960fd2e8aa75c76f692b36e6cf3c" "85e6bb2425cbfeed2f2b367246ad11a62fb0f6d525c157038a0d0eaaabc1bfee" "8543b328ed10bc7c16a8a35c523699befac0de00753824d7e90148bca583f986" "6271fc9740379f8e2722f1510d481c1df1fcc43e48fa6641a5c19e954c21cc8f" "50d07ab55e2b5322b2a8b13bc15ddf76d7f5985268833762c500a90e2a09e7aa" "ad16a1bf1fd86bfbedae4b32c269b19f8d20d416bd52a87cd50e355bf13c2f23" "100eeb65d336e3d8f419c0f09170f9fd30f688849c5e60a801a1e6addd8216cb" "4feee83c4fbbe8b827650d0f9af4ba7da903a5d117d849a3ccee88262805f40d" "6daa09c8c2c68de3ff1b83694115231faa7e650fdbb668bc76275f0f2ce2a437" "93268bf5365f22c685550a3cbb8c687a1211e827edc76ce7be3c4bd764054bad" "04790c9929eacf32d508b84d34e80ad2ee233f13f17767190531b8b350b9ef22" "1d079355c721b517fdc9891f0fda927fe3f87288f2e6cc3b8566655a64ca5453" "760ce657e710a77bcf6df51d97e51aae2ee7db1fba21bbad07aab0fa0f42f834" "cedd3b4295ac0a41ef48376e16b4745c25fa8e7b4f706173083f16d5792bb379" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "764a38f19474837bfa13ae177588cba0f6c3b2be558166efb7e1c3abc32db520" "5b24babd20e58465e070a8d7850ec573fe30aca66c8383a62a5e7a3588db830b" "2d6aae041fec67e3497d9d640617b600806c1e6cc9404cff3a2fbc6eda3f154e" "10e231624707d46f7b2059cc9280c332f7c7a530ebc17dba7e506df34c5332c4" "e84539eede64a272c84f3175f3e0aa7d09507a1dd0e85ca46725865dca779e1c" "957e1b72c5a39765c152d5967258463efde7387f177fcb72b5a8615b6d4b916a" "d55df21873ba9058e1bd85e6142c52f4c3b054cc9b3728e99e1e99ea3bcb3728" "e9b6fc5677c8e7f13fc681758cd6b0765ceaecdd7b706da156c30383852f7387" "3c837ab6df2be9c7c96bf0cc4c9cc71b2389343a68b23eb830f532639a170ae0" "bffba7a258ddd175fd85389ad2f472afe5cba8bfb9f5b723ae0c34ce290a3c09" "44eec3c3e6e673c0d41b523a67b64c43b6e38f8879a7969f306604dcf908832c" default)))
@@ -17,6 +17,7 @@
  '(mac-option-modifier (quote meta))
  '(mac-right-option-modifier nil)
  '(markdown-command "/Users/debajita/.rbenv/shims/octodown --raw")
+ '(org-hide-emphasis-markers t)
  '(package-selected-packages
    (quote
     (multiple-cursors helm-projectile projectile yaml-mode counsel swiper org-bullets helm-org-rifle deft pabbrev undo-tree typescript-mode gotham-theme neotree expand-region crux aggressive-indent helm-google easy-kill)))
@@ -31,4 +32,5 @@
  '(helm-source-header ((t (:background "#22083397778B" :foreground "white" :weight bold :height 1.5 :family "Verlag"))))
  '(org-level-1 ((t (:foreground "#83a598" :weight bold :height 2.4 :family "Verlag"))))
  '(org-level-2 ((t (:foreground "#fabd2f" :weight bold :height 1.8 :family "Verlag"))))
- '(org-level-3 ((t (:foreground "#d3869b" :weight bold :height 1.5 :family "Verlag")))))
+ '(org-level-3 ((t (:foreground "#d3869b" :weight bold :height 1.5 :family "Verlag"))))
+ '(org-link ((t (:inherit fixed-pitch)))))

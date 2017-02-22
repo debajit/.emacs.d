66fb071bf23c3e04fe146ecbc9a9649f75b5495a
Debajit Adhikary <debajit@debajit.com>
2017-02-21 16:37:07 -0800

    Update fixed-pitch face to remove background color

--- custom.el
+++ custom.el
@@ -30,6 +30,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fixed-pitch ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 161 :width normal :foundry "nil" :family "Consolas"))))
  '(helm-source-header ((t (:background "#22083397778B" :foreground "white" :weight bold :height 1.5 :family "Verlag"))))
  '(org-level-1 ((t (:foreground "#83a598" :weight bold :height 2.4 :family "Verlag"))))
  '(org-level-2 ((t (:foreground "#fabd2f" :weight bold :height 1.8 :family "Verlag"))))

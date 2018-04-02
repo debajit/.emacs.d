12eddc177c3f75c08183581e26c903de980df647
Debajit Adhikary <debajit@debajit.com>
2018-04-02 01:23:20 -0700

    Update shortcut to jump to method

--- init.el
+++ init.el
@@ -732,7 +732,7 @@
   :ensure t
   :bind (("M-x" . helm-M-x)
          ("s-SPC" . helm-mini)          ; List buffers, like C-x b
-         ("s-i" . helm-semantic-or-imenu) ; Jump to method
+         ("s-i" . helm-imenu)           ; Jump to method
          ("s-b" . helm-bookmarks))
   :init
   (setq helm-truncate-lines t)

bd1ce4a478c963af5ae17b397b56f35cfc0fc8c2
Debajit Adhikary <debajit@debajit.com>
2018-07-11 10:46:54 -0700

    Add date shortcuts for today and yesterday

--- init.el
+++ init.el
@@ -309,6 +309,26 @@
 (global-set-key (kbd "M-[") (lambda () (interactive) (shrink-window-horizontally 20)))
 
 
+;;----------------------------------------------------------------------
+;; Date shortcuts
+;;----------------------------------------------------------------------
+
+;; Yesterday’s date
+(global-set-key (kbd "<f9>")
+                (lambda ()
+                  "Insert yesterday’s date in the format: Sunday, January 1, 2018"
+                  (interactive)
+                  (insert (format-time-string "%A, %B %d, %Y"
+                                              (time-subtract (current-time) (* 24 60 60))))))
+
+;; Today’s date
+(global-set-key (kbd "<f10>")
+                (lambda ()
+                  "Insert today’s date in the format: Sunday, January 1, 2018"
+                  (interactive)
+                  (insert (format-time-string "%A, %B %d, %Y"))))
+
+
 ;;--------------------------------------------------------------------
 ;; Getting Things Done.
 ;;

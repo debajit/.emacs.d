0e4878e367cbc2fbec211c19a1df5998f306fc21
Debajit Adhikary <debajit@debajit.com>
2018-08-09 10:58:43 -0700

    Add keybindings to open links in Org mode easily
    Makes opening things consistent with other modes.

--- navigation.el
+++ navigation.el
@@ -1,1 +1,5 @@
 (global-set-key (kbd "M-i") 'imenu)
+
+;; Open file or link in Org mode with the same key shortcut
+(define-key org-mode-map (kbd "s-.") 'org-open-at-point)
+(define-key org-mode-map (kbd "s-,") 'org-mark-ring-goto)

8aa8db60dca16416a74f31a8d215f2c6072c57cf
Debajit Adhikary <debajit@debajit.com>
2018-10-31 18:03:15 -0700

    Update macro for standup so that Friday’s are calculated automatically

--- date-time.el
+++ date-time.el
@@ -2,17 +2,34 @@
 ;; Date shortcuts
 ;;----------------------------------------------------------------------
 
-;; Yesterday’s date
+(defun date-label (&optional time)
+  (format-time-string "%A, %B %-d, %Y" time))
+
+(defun days-ago-time (days-ago)
+  (time-subtract (current-time) (* days-ago 24 60 60)))
+
+(defun today-date-label ()
+  (date-label))
+
+(defun yesterday-date-label ()
+  (date-label (days-ago-time 1)))
+
+(defun last-working-day-date-label ()
+ (date-label
+  (if (string= (format-time-string "%u") "1")
+      (days-ago-time 3)
+    (days-ago-time 1))))
+
+;; Last working day label
 (global-set-key (kbd "<f9>")
                 (lambda ()
-                  "Insert yesterday’s date in the format: Sunday, January 1, 2018"
+                  "Insert the last working day’s date in the format: Sunday, January 1, 2018"
                   (interactive)
-                  (insert (format-time-string "%A, %B %d, %Y"
-                                              (time-subtract (current-time) (* 24 60 60))))))
+                  (insert (last-working-day-date-label))))
 
 ;; Today’s date
 (global-set-key (kbd "<f10>")
                 (lambda ()
                   "Insert today’s date in the format: Sunday, January 1, 2018"
                   (interactive)
-                  (insert (format-time-string "%A, %B %d, %Y"))))
+                  (insert (today-date-label))))

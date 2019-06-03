a051999f9a92bfc3adde32cc1a2603e57e08ae55
Debajit Adhikary <debajit@debajit.com>
2019-06-03 15:08:40 -0700

    Add conf mode to quick mode-switcher hydra

--- playground.el
+++ playground.el
@@ -56,28 +56,29 @@ that was stored with ska-point-to-register."
                                     :hint nil
                                     )
   "
- ^Writing^          ^Programming^         ^Plain^                 ^Formatting
+ ^Writing^          ^Programming^         ^Other^                 ^Formatting
 -------------    ---------------    -----------------     -------------
- _o_: Org           _e_: Elixir           _f_: Fundamental        _Q_: Electric Quotes
- _m_: Markdown      _r_: Ruby             ^ ^                     _a_: Auto-fill Mode
+ _o_: Org           _e_: Elixir           _c_: Conf Mode        _Q_: Electric Quotes
+ _m_: Markdown      _r_: Ruby             _f_: Fundamental      _a_: Auto-fill Mode
  ^ ^                _j_: JavaScript
  ^ ^                _g_: GraphQL
  ^ ^                _l_: Emacs-Lisp
  ^ ^                _s_: Shell-Script
 "
+  ("Q" electric-quote-mode)
+  ("R" Buffer-menu-isearch-buffers-regexp :color blue)
+  ("a" auto-fill-mode)
+  ("c" conf-mode)
+  ("e" elixir-mode)
+  ("f" fundamental-mode)
+  ("g" graphql-mode)
+  ("j" js2-mode)
+  ("l" emacs-lisp-mode)
   ("m" gfm-mode)
   ("o" org-mode)
-  ("e" elixir-mode)
   ("r" ruby-mode)
-  ("j" js2-mode)
-  ("g" graphql-mode)
-  ("l" emacs-lisp-mode)
   ("s" shell-script-mode)
-  ("f" fundamental-mode)
-  ("Q" electric-quote-mode)
-  ("a" auto-fill-mode)
-  ("I" Buffer-menu-isearch-buffers :color blue)
-  ("R" Buffer-menu-isearch-buffers-regexp :color blue)
+
   ("q" nil "Quit")
   ;; ("q" quit-window "quit" :color blue)
   )

1a6b04496020fac65c296ecd6a5c1c21a270c197
Debajit Adhikary <debajit@debajit.com>
2022-04-26 15:06:11 -0700

    Cleanup and simplify functions behind date keybindings

--- date-time.el
+++ date-time.el
@@ -1,61 +1,35 @@
-;;----------------------------------------------------------------------
+;;
 ;; Date shortcuts
-;;----------------------------------------------------------------------
+;;
+;;       F9 - Insert today’s date
+;;      F10 - Insert tomorrow’s date
+;; Shift+F9 - Insert yesterday’s date
+;;
 
-(defun date-label (&optional time)
+(defun day-label (&optional time)
+  ;; %A = Full day of week e.g. Wednesday
+  ;; %B = Full month name
+  ;; %-d = numeric day of the month, not padded
+  ;; %Y = year
   (format-time-string "%A, %B %-d, %Y" time))
 
-(defun days-ago-time (days-ago)
-  (time-subtract (current-time) (* days-ago 24 60 60)))
+(defun tomorrow-time ()
+  (time-add (current-time) (* 24 60 60)))
 
-(defun today-date-label ()
-  (date-label))
+(defun yesterday-time ()
+  (time-subtract (current-time) (* 24 60 60)))
 
-(defun yesterday-date-label ()
-  (date-label (days-ago-time 1)))
-
-;; (defun last-working-day-date-label ()
-;;  (date-label
-;;   (if (string= (format-time-string "%u") "1")
-;;       (days-ago-time 3)
-;;     (days-ago-time 1))))
-
-
-(defun standup-yesterday-label ()
-  (if (string= (format-time-string "%u") "1")
-      (concat "**FRIDAY** " (format-time-string "(%B %-d, %Y)" (days-ago-time 3)))
-    (concat "**YESTERDAY** (" (yesterday-date-label) ")")
-    ))
-
-
-(defun standup-today-label ()
-  (concat "**TODAY** (" (today-date-label) ")"))
-
-;; Yesterday date label
 (global-set-key (kbd "<f9>")
                 (lambda ()
-                  "Insert the last working day’s date in the format: Sunday, January 1, 2018"
                   (interactive)
-                  (insert (yesterday-date-label))))
+                  (insert (day-label))))
 
-;; Today’s date
 (global-set-key (kbd "<f10>")
                 (lambda ()
-                  "Insert today’s date in the format: Sunday, January 1, 2018"
                   (interactive)
-                  (insert (today-date-label))))
+                  (insert (day-label (tomorrow-time)))))
 
-
-;; Standup: “Yesterday” label
 (global-set-key (kbd "<S-f9>")
                 (lambda ()
-                  "Insert the last working day’s date in the format: Sunday, January 1, 2018"
                   (interactive)
-                  (insert (standup-yesterday-label))))
-
-;; Standup: Today label
-(global-set-key (kbd "<S-f10>")
-                (lambda ()
-                  "Insert today’s date in the format: Sunday, January 1, 2018"
-                  (interactive)
-                  (insert (standup-today-label))))
+                  (insert (day-label (yesterday-time)))))

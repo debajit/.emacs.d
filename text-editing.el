44b2d43533ad48e2058afeb63d32dbc30f28338f
Debajit Adhikary <debajit@debajit.com>
2022-08-22 16:40:44 -0700

    Add title case support

--- text-editing.el
+++ text-editing.el
@@ -1,3 +1,20 @@
+;; Title Case Support. See
+;;
+;; - “Title Case in Emacs”
+;;   https://jblevins.org/log/titlecase
+;;
+;; - A refactoring of John Gruber’s Title Case Program
+;;   https://github.com/ap/titlecase
+;;
+;; - “Title Case” by John Gruber
+;;   https://daringfireball.net/2008/05/title_case
+;;
+;; Melpa’s titlecase is a possibly better implementation that can
+;; still use external programs.
+;; https://codeberg.org/acdw/titlecase.el
+;;
+(require 'titlecase)
+
 (global-set-key (kbd "s-z") 'undo)
 
 ;; Jump out of strings, parens, brackets etc.

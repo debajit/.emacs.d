a315322541255a2823a02e3b9e0d1de675a1e581
Debajit Adhikary <debajit@debajit.com>
2022-10-28 12:07:02 -0700

    Jump to matching parenthesis by pressing % (like in Vim)

--- navigation.el
+++ navigation.el
@@ -1,3 +1,16 @@
+;; Jump to matching parenthesis by pressing % (like in Vim). See
+;; https://www.gnu.org/software/emacs/manual/html_node/efaq/Matching-parentheses.html
+
+(global-set-key "%" 'match-paren)
+
+(defun match-paren (arg)
+  "Go to the matching paren if on a paren; otherwise insert %."
+  (interactive "p")
+  (cond ((looking-at "\\s(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
+
+
 ;; C-:  Jump to any character on the window
 ;; (global-unset-key "\C-'")
 (global-set-key (kbd "M-s-j") 'avy-goto-char)

1cd31366127ae2e890c0ccbedf6e69b2814b5c41
Debajit Adhikary <debajit@debajit.com>
2023-09-09 17:48:15 -0700

    JS: Update config

--- javascript.el
+++ javascript.el
@@ -1,13 +1,21 @@
 (setq js-indent-level 2)
 
-;; JS2 Mode
-(use-package js2-mode
-  :ensure t
-  :mode (("\\.js$" . js2-mode)
-         ("\\.es6$" . js2-mode))
-  :init
-  (setq js2-strict-missing-semi-warning nil)
-  (setq js2-missing-semi-one-line-override t)
-  )
+
+;; ;; JS2 Mode
+;; (use-package js2-mode
+;;   :ensure t
+;;   :mode (("\\.js$" . js2-mode)
+;;          ("\\.es6$" . js2-mode))
+
+;;   :config
+;;   (add-hook 'js-mode-hook 'js2-minor-mode)
+
+;;   :init
+;;   (setq js2-strict-missing-semi-warning nil)
+;;   (setq js2-missing-semi-one-line-override t)
+;;   )
+
+;; (with-eval-after-load 'js
+;;   (add-hook 'js-mode-hook 'js2-minor-mode))
 
 ;; (use-package tide)

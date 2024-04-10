61b085372113ed54bd94849f651df39f10ddabc7
Debajit Adhikary <debajit@debajit.com>
2024-04-10 10:46:46 -0700

    Calendar: Add keys to quickly open Calendar, World Clock
    See README for details

--- calendar.el
+++ calendar.el
@@ -1,3 +1,18 @@
+;; Set longitude and latitude, for sunrise and sunset calculations
 (setq calendar-latitude 47.6)
 (setq calendar-longitude -122.2)
 (setq calendar-location-name "Bellevue, WA")
+
+(global-set-key (kbd "C-s-c") 'calendar)     ; Super+Control+c => Calendar
+
+;; World clock.
+;; See https://emacsredux.com/blog/2024/03/11/tracking-world-time-with-emacs/
+(setq world-clock-time-format "%a %e %b %I:%M %p %Z")
+(setq world-clock-list
+      '(("America/Los_Angeles" "Seattle")
+        ("America/New_York" "New York")
+        ("Europe/London" "London")
+        ("Asia/Calcutta" "Mumbai")
+        ))
+
+(global-set-key (kbd "C-s-w") 'world-clock)  ; Super+Control+w => World clock

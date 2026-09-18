f5fd350da1ca62f040c4add6a6cdb8a5e6c59c67
Debajit Adhikary <debajit@debajit.com>
2026-09-17 23:26:34 -0700

    packages: avoid interactive shell for PATH setup

--- init.el
+++ init.el
@@ -350,6 +350,8 @@
 (use-package exec-path-from-shell
   :ensure t
   :init
+  (setq exec-path-from-shell-arguments '("-l"))
+  :config
   (when (memq window-system '(mac ns x))
     (exec-path-from-shell-initialize)))
 

606cce2b6d4d4ccc68331bcf90bdc5cbd2ab825a
Debajit Adhikary <debajit@debajit.com>
2026-09-24 23:20:48 -0700

    file-jump-keys: update files

--- file-jump-keys.el
+++ file-jump-keys.el
@@ -6,14 +6,13 @@
 ;;
 
 (global-set-key (kbd "C-s-;") (lambda () (interactive) (find-file "~/Documents/draft.org")))
+(global-set-key (kbd "C-s-e") (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
+(global-set-key (kbd "M-E")   (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
 (global-set-key (kbd "C-s-a") (lambda () (interactive) (find-file "~/Archive/Knowledge/Notes/20221202172447-algorithms_and_data_structures.org")))
 (global-set-key (kbd "C-s-s") (lambda () (interactive) (find-file "~/Archive/Knowledge/Notes/20221126000441-system_software_design_interviews.org")))
-(global-set-key (kbd "C-s-e") (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
-(global-set-key (kbd "C-s-i") (lambda () (interactive) (find-file inbox-tasks-file)))
 (global-set-key (kbd "C-s-m") (lambda () (interactive) (find-file "~/Archive/Knowledge/Notes/Morning_Routine.org")))
-(global-set-key (kbd "C-s-l") (lambda () (interactive) (find-file "~/WorkDocs/Documents/DEVELOPMENT.org")))
-(global-set-key (kbd "C-s-t") (lambda () (interactive) (find-file "~/Projects/Code/Drafts/test.cc")))
-(global-set-key (kbd "M-E")   (lambda () (interactive) (find-file "~/.emacs.d/init.el")))
+(global-set-key (kbd "C-s-i") (lambda () (interactive) (find-file inbox-tasks-file)))
 
-;; (global-set-key (kbd "<C-s-268632073>") (lambda () (interactive) (find-file inbox-tasks-file)))
-;; (global-set-key (kbd "<C-s-268632074>") (lambda () (interactive) (find-file work-journal-file)))
+(global-set-key (kbd "C-s-l") (lambda () (interactive) (find-file "~/Work/Docs/Documents/DEVELOPMENT.org")))
+(global-set-key (kbd "C-s-r") (lambda () (interactive) (find-file "~/Work/Knowledge/WorkNotes/Planning/Sprint Retrospective.org")))
+(global-set-key (kbd "C-s-w") (lambda () (interactive) (find-file "~/Projects/Tasks/org-agenda/work-log.org")))
